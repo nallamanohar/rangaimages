c1d2696b1f50f376acdd15f780c0a251f938ed91
nchandankumar <chandankumar1822001@gmail.com>
2026-05-31 18:28:49 +0530

    bug fix: booking route

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,11 @@ const dotenv = require("dotenv");
 const express = require("express");
 const connectDB = require("./dbconnection");
 
-process.on("uncaughtException", (err) => {
-  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  process.exit(1);
-});
+// process.on("uncaughtException", (err) => {
+//   console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+//   console.log(err.name, err.message);
+//   process.exit(1);
+// });
 
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
@@ -18,10 +18,10 @@ const server = app.listen(port, () => {
   console.log(`💻💻💻App running on port ${port}...`);
 });
 
-process.on("unhandledRejection", (err) => {
-  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
-  console.log(err.name, err.message);
-  server.close(() => {
-    process.exit(1);
-  });
-});
+// process.on("unhandledRejection", (err) => {
+//   console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
